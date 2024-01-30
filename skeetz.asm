5632e8bb7d04dc498c7c6fa6c802e55fdea66802
Maria A. Dobrovolskiя <70070417+disquxxn@users.noreply.github.com>
2024-01-30 14:27:35 -0500

    Create skeetz.asm

---
+++ skeetz.asm
@@ -0,0 +1,3 @@
+hey um listen im sorry if i weirded you out. I didn't mean to I just missed you, no ill intention i promise
+i will keep in touch just thru ur mom so i wont bother you. 
+i truly hope every thing works out for the better 4 u. 
